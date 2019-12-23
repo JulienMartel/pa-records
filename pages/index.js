2067869bcc6d25ca69255b04632c6cf06fe43ba7
Martel <x-jmarte@ad.cityofwpg.org>
2019-12-23 16:16:42 -0600

    init

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,81 @@
 import React from 'react'
+import EmblaCarouselReact from 'embla-carousel-react'
 import Head from 'next/head'
-import Nav from '../components/nav'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
+class EmblaCarouselComponent extends React.Component {
+  componentDidMount() {
+    this.embla.on('select', () => {
+      console.log(
+        `Current index is ${this.embla.selectedScrollSnap()}`,
+      )
+    })
+  }
+ 
+  render() {
+    return (
+      <>
+        <Head>
+          {/* #D9DBBC  is bone-white */}
+          <style>@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap');</style> 
+        </Head>
+        <EmblaCarouselReact
+          emblaRef={c => (this.embla = c)}
+          options={{ loop: false }}
         >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
+          <div style={{ display: 'flex'}} className="cont">
+            <div className="slide" style={{ flex: '0 0 100%' }}>
+              <div className="this">
+                <a href="https://google.ca">C A I D J O N E S</a>
+              </div>
+            </div>
+            <div className="slide" style={{ flex: '0 0 100%' }}>Slide 2</div>
+            <div className="slide" style={{ flex: '0 0 100%' }}>Slide 3</div>
+          </div>
+        </EmblaCarouselReact>
+        <button onClick={() => this.embla.scrollPrev()}>Prev</button>
+        <button onClick={() => this.embla.scrollNext()}>Next</button>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+        <style jsx global>{`
+        * {
+          font-family: 'Montserrat', sans-serif;
+        }
+        html {
+          background-color: #363636;
+        }
+        body {
+          margin: 0;
+          padding 0;
+        }
 
-export default Home
+        .cont {
+          
+        }
+
+        .slide{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+         }
+
+         .this {
+           border: 10px solid #CCA43B;
+           border-radius: 40px;
+           background-color: #242F40;
+           width: 90%;
+           height: 80vh;
+           padding: 20px;
+           margin: 10px 0;
+           display: flex;
+           justify-content: center;
+           align-items: center;
+           font-size: 2em;
+           font-weight:600;
+         }
+        `}</style>
+      </>
+    )
+  }
+}
+
+
+export default EmblaCarouselComponent
