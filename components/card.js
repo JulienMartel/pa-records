d4aeed99f08df66d3c235b46904c9ed7b1c12c28
ju <julienbmartel@gmail.com>
2020-06-08 22:34:56 -0500

    changes

--- components/card.js
+++ components/card.js
@@ -1,76 +1,71 @@
 import React from 'react'
 import Nav from '../components/nav'
 
-class Twitter extends React.Component {
-    render() {
-        if (this.props.noTwitter) {
-            return <></>;
-        }
-        else {
-            return <a className="twitter" href={this.props.link}><img src="/twitter.svg" style={{width: "30px",}}></img></a>
-        }
-    }
-}
-
-
 class Card extends React.Component {
-    render() {
+  render() {
+    const { 
+      isFirst,
+      isLast,
+      embla,
+      name,
+      twitter,
+      instagram,
+      youtube,
+      soundcloud,
+      spotify,
+      bandcamp,
+    } = this.props
+    return (
+      <div className="box">
+        <Nav isFirst={isFirst} isLast={isLast} embla={embla}></Nav>
+        <h3 className="name">{name}</h3>
+        {twitter && <a className="twitter" href={twitter}><img src="/twitter.svg" style={{width: "30px",}}></img></a>}
+        {instagram && <a className="instagram" href={instagram}><img src="/instagram.svg" style={{width: "30px",}}></img></a>}
+        {youtube && <a className="youtube" href={youtube}><img src="/youtube.svg" style={{width: "30px",}}></img></a>}
+        {soundcloud && <a className="soundcloud" href={soundcloud}><img src="/soundcloud.svg" style={{width: "30px",}}></img></a>}
+        {spotify && <a className="spotify" href={spotify}><img src="/spotify.svg" style={{width: "30px",}}></img></a>}
+        {bandcamp && <a className="bandcamp" href={bandcamp}><img src="/bandcamp.svg" style={{width: "30px",}}></img></a>}
         
-        if (this.props.noTwitter) {
-            let twitter  = <></>;
-        }
-        else {
-            let twitter = <a className="twitter" href={this.props.twitter}><img src="/twitter.svg" style={{width: "30px",}}></img></a>
-        }
+        <style global jsx>{`
+          .back, button {
+            position: absolute;
+            background-color: #9f9f9f00;
+            z-index: 10;
+            border: 0;
+            height: 100px;
+            top: calc(50% - 50px);
+            left: 5px;
+            color: white;
+          }
+          button.next{
+            right: 5px;
+            left: unset;
+          }
 
-        return (
-            <div className="box">
-                <Nav isFirst={this.props.isFirst} isLast={this.props.isLast} embla={this.props.embla}></Nav>
-                <h3 className="name">{this.props.name}</h3>
-                <Twitter noTwitter={this.props.noTwitter} link={this.props.twitter}/>
-                <a className="instagram" href={this.props.instagram}><img src="/instagram.svg" style={{width: "30px",}}></img></a>
-                <a className="youtube" href={this.props.youtube}><img src="/youtube.svg" style={{width: "30px",}}></img></a>
-                <a className="soundcloud" href={this.props.soundcloud}><img src="/soundcloud.svg" style={{width: "30px",}}></img></a>
-                <style global jsx>{`
-                    .back, button {
-                        position: absolute;
-                        background-color: #9f9f9f00;
-                        z-index: 10;
-                        border: 0;
-                        height: 100px;
-                        top: calc(50% - 50px);
-                        left: 5px;
-                        color: white;
-                    }
-                    button.next{
-                        right: 5px;
-                        left: unset;
-                    }
+          .name {
+            font-weight:400;
+            font-size: 1em;
+          }
 
-                    .name {
-                        font-weight:400;
-                        font-size: 1em;
-                    }
-
-                    .box {
-                        width: 60vw;
-                        height: 55vh;
-                        background-color: black;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        padding: 20px;
-                        flex-direction: column;
-                        position: relative;
-                      }
-              
-                      .box a {
-                        margin: 20px;
-                      }
-                `}</style>
-            </div>
-        )
-    }
+          .box {
+            width: 60vw;
+            min-height: 55vh;
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+            flex-direction: column;
+            position: relative;
+            }
+      
+            .box a {
+            margin: 20px;
+            }
+        `}</style>
+      </div>
+    )
+  }
 }
 
 export default Card
--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,21 @@ import Card from '../components/card'
 import Head from 'next/head'
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { width: 0, height: 0 };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  state = { 
+    width: 0, 
+    height: 0 
   }
   
-  componentDidMount() {
+  componentDidMount = () => {
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
-
-    this.embla.on('select', () => {
-      console.log(
-        `Current index is ${this.embla.selectedScrollSnap()}`,
-      )
-    })
   }
   
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
   
-  updateWindowDimensions() {
+  updateWindowDimensions = () => {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
@@ -36,6 +29,14 @@ class Home extends React.Component {
       <Head>
         <title>PA Records</title>
           <link rel="shortcut icon" href="/favicon.png" />
+          <meta property="og:title" content="Pay Attention Creations" />
+          <meta property="og:description" content="PA Records" />
+          <meta property="og:image" content="/ogimg.png" />
+          <meta property="og:url" content="https://toodistracted.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta property="og:site_name" content="Pay Attention" />
+          <meta name="twitter:image:alt" content="Pay Attention text" />
       </Head>
         <EmblaCarouselReact
           emblaRef={c => (this.embla = c)}
@@ -50,8 +51,9 @@ class Home extends React.Component {
                   embla={this.embla}
                   twitter="https://twitter.com/AbOrlglnaI"
                   soundcloud="https://soundcloud.com/caidjones"
-                  youtube="https://www.youtube.com/watch?v=HUJqpuuMEPk"
-                  instagram="https://instagram.com/caidjones"
+                  youtube="https://www.youtube.com/channel/UCcxlsh7kUPHeF7gYM1WYiag/"
+                  spotify="https://open.spotify.com/artist/69OZCcvont06MaZH5KeLxz"
+                  bandcamp="https://caidjones.bandcamp.com/releases"
                 ></Card>
               </div>
             </div>
@@ -73,8 +75,7 @@ class Home extends React.Component {
                   name="Sunday Knight" 
                   isLast={true} 
                   embla={this.embla}
-                  twitter=""
-                  noTwitter={true}
+                  twitter={null}
                   soundcloud="https://soundcloud.com/sundayknight"
                   youtube="https://www.youtube.com/watch?v=y49FmjrI-U8"
                   instagram="https://instagram.com/sundayknight47"
@@ -88,82 +89,82 @@ class Home extends React.Component {
           Pay Attention
         </div>
 
-        <style jsx global>{`
-        @import url('');
-        @font-face {
-            font-family:"Montserrat Alternates";
-            src: url("/font/MontserratAlternates-Medium.ttf")
-        }
-      
-        * {
-          font-family: 'Montserrat Alternates', sans-serif;
-        }
-        html {
-          background-color: black;
-        }
-        body {
-          margin: 0;
-          padding 0;
-          postion: relative;
-        }
-
-        a {
-          text-decoration: none;
-          color: white;
-        }
-
-        .slide{
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .this {
-          color: white;
-          background-size: 50%;
-          background-position: center;
-          width: ${this.state.width}px;
-          height: ${this.state.height}px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          font-size: 1.5em;
-        }
-        .josh {
-          background-image: url("/josh500.png")
-        }
-        .dion {
-          background-image: url("/dion3.png");
-          background-size: 50%;
-          background-position: unset;
-        }
-        .caid {
-          background-image: url("/cayden500.png");
-        }
-        @media (max-width: 600px) {
-          .this.dion {
-            background-size: 100%;
+        <style global jsx>{`
+          @import url('');
+          @font-face {
+              font-family:"Montserrat Alternates";
+              src: url("/font/MontserratAlternates-Medium.ttf")
           }
-        }
+        
+          * {
+            font-family: 'Montserrat Alternates', sans-serif;
+          }
+          html {
+            background-color: black;
+          }
+          body {
+            margin: 0;
+            padding 0;
+            postion: relative;
+          }
 
-        @media (min-width: 800px) {
-          .this.josh, .this.caid {
-            background-size: 35%;
+          a {
+            text-decoration: none;
+            color: white;
+          }
+
+          .slide{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .this {
+            color: white;
+            background-size: 50%;
+            background-position: center;
+            width: ${this.state.width}px;
+            height: ${this.state.height}px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 1.5em;
+          }
+          .josh {
+            background-image: url("/josh500.png")
+          }
+          .dion {
+            background-image: url("/dion3.png");
+            background-size: 50%;
+            background-position: unset;
+          }
+          .caid {
+            background-image: url("/cayden500.png");
+          }
+          @media (max-width: 600px) {
+            .this.dion {
+              background-size: 100%;
+            }
+          }
+
+          @media (min-width: 800px) {
+            .this.josh, .this.caid {
+              background-size: 35%;
+
+            }
+          }
+
+          .title {
+            top: 20px;
+            left 20px;
+            position: absolute;
+            color: white;
+            font-size: 1.5vh;
+            background-color: black;
+            padding: 5px;
+            font-weight:700;
 
           }
-        }
-
-        .title {
-          top: 20px;
-          left 20px;
-          position: absolute;
-          color: white;
-          font-size: 1.5vh;
-          background-color: black;
-          padding: 5px;
-          font-weight:700;
-
-        }
         `}</style>
       </>
     )
